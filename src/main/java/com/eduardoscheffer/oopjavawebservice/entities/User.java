0498916e9bc2d15f5322d427ac210977a18eb322
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-10-03 18:30:29 -0300

    configured application.properties & added JPA dependency

--- src/main/java/com/eduardoscheffer/oopjavawebservice/entities/User.java
+++ src/main/java/com/eduardoscheffer/oopjavawebservice/entities/User.java
@@ -1,5 +1,6 @@
 package com.eduardoscheffer.oopjavawebservice.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -9,8 +10,12 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
