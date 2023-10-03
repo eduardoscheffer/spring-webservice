6dbdd15ddb1ca2d3481d723ca387b362e6ccc4cc
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-10-03 13:10:55 -0300

    Project created

---
+++ src/main/java/com/eduardoscheffer/oopjavawebservice/OopJavaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoscheffer.oopjavawebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopJavaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopJavaWebServiceApplication.class, args);
+	}
+
+}
